d61b5e4c9399b9adab31c44239ae69f8c601d61d
rosenrose <crazytempler@gmail.com>
2022-05-07 20:55:48 +0900

    d

--- pages/movies/[...paths].js
+++ pages/movies/[...paths].js
@@ -10,16 +10,16 @@ export default function Detail(props) {
   // console.log("router", router, "props", props);
   // const thumbnail = router.query.thumbnail;
 
-  const { data: thumbData } = useQuery(id, () =>
-    fetch(`https://asia-northeast3-get-youtube-thumbnail.cloudfunctions.net/thumbnail?id=${id}`)
-      .then((response) => response.text())
-      .then((thumbnail) => ({ id, thumbnail }))
-  );
-
   const { isLoading, data: movie } = useQuery(["info", id], () =>
     fetch(
       `https://www.googleapis.com/youtube/v3/videos?key=${process.env.API_KEY}&id=${id}&part=snippet,contentDetails`
     ).then((response) => response.json())
+  );
+
+  const { data: thumbData } = useQuery(id, () =>
+    fetch(`https://asia-northeast3-get-youtube-thumbnail.cloudfunctions.net/thumbnail?id=${id}`)
+      .then((response) => response.text())
+      .then((thumbnail) => ({ id, thumbnail }))
   );
 
   return (
